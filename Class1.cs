9eb980d7ce4589133df28b6b09987b05d180cb0e
d11416206-cloud <d11416206@gs.takming.edu.tw>
2026-05-15 10:09:45 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public interface IName
+{
+	string GetName();
+}
+
+public class Person : IName
+{
+	public string GetName()
+	{
+		return "Person"
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace class_0515_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IName p1 = new Person();
+            Console.WriteLine(p1.GetName());
+        }
+    }
+}
